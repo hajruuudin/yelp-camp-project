194917bb3cfabdd0ba5a765b589aa47954669156
hajruuudin <hajruuudin@gmail.com>
2024-09-22 21:46:07 +0200

    002: Basic Styles and CRUD Clean Up

--- seeds/seed.js
+++ seeds/seed.js
@@ -19,9 +19,13 @@ const seedDb = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price =  Math.floor(Math.random() * 30 + 10);
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: `https://picsum.photos/960/540`,
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nobis corporis ipsum explicabo neque quidem deserunt porro itaque ducimus a modi id, aut esse est dicta quam officia iure? Earum.",
+            price: price
         })
         await camp.save();
     }
